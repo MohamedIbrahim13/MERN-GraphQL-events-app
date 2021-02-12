6830b4dffae4659e287cea118fc74062b999d44d
mohamed <mohamed.mohamed.yossef@gmail.com>
2021-02-12 14:10:34 +0200

    linting eventlist component and modal component

--- client/src/components/EventsList.js
+++ client/src/components/EventsList.js
@@ -1,7 +1,7 @@
 import { useQuery, useMutation } from "@apollo/client";
 import { EVENTS_QUERY, CANCEL_EVENT } from "../queries/query";
-import Spinner from './Spinner';
-import moment from 'moment';
+import Spinner from "./Spinner";
+import moment from "moment";
 
 const EventsList = () => {
   const { data, loading } = useQuery(EVENTS_QUERY);
@@ -15,23 +15,28 @@ const EventsList = () => {
         ) : (
           data.events.map((event) => {
             return (
-              <li
-                className="mb-4"
-                key={event.id}
-              
-              >
-			  <div className="card">
-  <div className="card-header">
-    {moment(Date(event.date).toString()).calendar()}
-  </div>
-  <div className="card-body">
-    <h5 className="card-title">{event.title}</h5>
-    <p className="card-text">{event.description}</p>
-	<p className="card-text">${event.price}</p>
-    <button className="btn btn-danger" onClick={()=>cancelEvent({variables:{id:event.id},refetchQueries:[{query:EVENTS_QUERY}]})}>Delete</button>
-  </div>
-</div>
-                
+              <li className="mb-4" key={event.id}>
+                <div className="card">
+                  <div className="card-header">
+                    {moment(Date(event.date).toString()).calendar()}
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-title">{event.title}</h5>
+                    <p className="card-text">{event.description}</p>
+                    <p className="card-text">${event.price}</p>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() =>
+                        cancelEvent({
+                          variables: { id: event.id },
+                          refetchQueries: [{ query: EVENTS_QUERY }],
+                        })
+                      }
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
               </li>
             );
           })
